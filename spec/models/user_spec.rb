4bdf53416bb81a7a6fa03df36a784629cbc58600
JW Shinn <jwshinn@gmail.com>
2010-03-08 11:00:17 -0800

    Finished first cut of the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,13 +14,23 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :email => "value for email"
-    }
+    @attr = { :name => "Example User", :email => "user@example.com" }
   end
+
+    
 
   it "should create a new instance given valid attributes" do
     User.create!(@valid_attributes)
   end
+
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
 end
