fff4898cda8b1c24d346518afb0786e1043de81c
JW Shinn <jwshinn@gmail.com>
2010-03-14 23:32:09 -0700

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20100307195215
+# Schema version: 20100308002627
 #
 # Table name: users
 #
@@ -9,8 +9,10 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
+require 'digest'
 
 class User < ActiveRecord::Base
+  attr_accessor :password
   attr_accessible :name, :email
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -19,6 +21,45 @@ class User < ActiveRecord::Base
   validates_length_of   :name, :maximum => 50
   validates_format_of   :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+
+  #Automatically create the virtual attribute 'password_confirmation'.
+  validates_confirmation_of :password
+  #Password validations.
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
+
+  before_save :encrypt_password
+
+    def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+    end
+
+    def self.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil  if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
 end
 
 
